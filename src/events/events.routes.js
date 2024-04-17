1e12d47bffb8b07dbe9e22c4f79f95c84ee5310f
osanchez <osanchez-2019490@kinal.edu.gt>
2024-04-17 10:56:00 -0600

    Schema echo

--- src/events/events.routes.js
+++ src/events/events.routes.js
@@ -1,1 +1,38 @@
-import 
+import mongoose from "mongoose";
+
+const EventsSchema = mongoose.Schema({
+    nameEvent: {
+        type: String,
+        required: [true, 'The event need one name']
+    },
+
+    descriptionEvent: {
+        type: String,
+        required: [true, 'The event need one description']
+    },
+
+    date: {
+        type: Date,
+        required: [true, 'The evenet need one date']
+    },
+
+    typeEvent: {
+        type: String,
+        enum: ['Conference', 'Wedding', 'Meeting', 'Other'],
+        required: [true, 'The event need one type']
+    },
+
+    resources: [{
+        nameResource: String,
+        amount: Number
+    }],
+
+    additionalServices: [String],
+
+    state: {
+        type: Boolean,
+        default: true
+    }
+
+
+})
