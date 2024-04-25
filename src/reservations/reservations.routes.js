cd30886a258c94c0e44f55f939329dddb6fedaa5
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-04-25 00:49:05 -0600

    agregamos ruta de post en routes

--- src/reservations/reservations.routes.js
+++ src/reservations/reservations.routes.js
@@ -1,22 +1,39 @@
-import { Router } from "express";
+import { Router } from 'express';
+import { body, param } from 'express-validator';
 import {
-    reservacionDelete,
-    reservacionPost,
-    reservacionPut,
-    reservacionGet,
-    getHabitacionByReservacion
-} from "./reservations.controller.js";
+    crearReservacion,
+    getReservaciones,
+    reservacionesPut,
+    reservacionDelete
+} from './reservations.controller.js';
+
+import {
+    validarCampos
+} from '../middlewares/validar-campos.js';  
+
+import{
+    validarCapacidad,
+    validarDisponibilidad,
+    validarFechar,
+    validarNumeroHuespedes,
+    validarUsuario
+} from '../helpers/db-validators.js';
 
 const router = Router();
 
-router.post("/", reservacionPost);
-
-router.put("/:id", reservacionPut);
-
-router.get("/", reservacionGet);
-
-router.delete("/:id", reservacionDelete);
-
-router.get("/:id/habitacion", getHabitacionByReservacion);
-
-export default router;
+router.post(
+    '/',
+    [
+        check("usuario", "User ID is required").not().isEmpty(),
+        check("habitacion", "Room ID is required").not().isEmpty(),
+        check("fechaInicio", "Start date is required").not().isEmpty(),
+        check("fechaFin", "End date is required").not().isEmpty(),
+        check("huespedes", "Number of guests is required").not().isEmpty(),
+        check("pago", "Payment is required").not().isEmpty(),
+        check("fechaInicio").custom(validarFechar),
+        check("fechaFin").custom(validarFechar),
+        check("huespedes").custom(validarNumeroHuespedes)
+    ],
+    validarCampos,
+    crearReservacion
+);
