3535df62c9ba6d5af8bf2c0a0c70dfae332f0e43
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-04-24 23:22:32 -0600

    add function of update to reservation

--- src/reservations/reservations.controller.js
+++ src/reservations/reservations.controller.js
@@ -47,3 +47,28 @@ export const getReservaciones = async (req, res= response) => {
     }
 };
 
+export const reservacionesPut = async (req, res = response) => {
+    try {
+        const { id } = req.params;
+        const { _id, usuario, ...resto } = req.body;
+
+        const reservacionExistente = await Reservacion.findById(id);
+        if(!reservacionExistente){
+            return res.status(404).json({ error: 'Reservación no encontrada' });
+        }
+
+        if(reservacionExistente.usuario.toString() !== req.usuario.id){
+            return res.status(401).json({ error: 'No tienes permiso para actualizar esta reservación'});
+        }
+
+        const reservacionActualizada = await Reservacion.findByIdAndUpdate(id, resto, { new: true });
+        
+        res.status(200).json({ msg: 'Reservación actualizada exitosamente', reservacion: reservacionActualizada});
+    } catch (error) {
+        console.error('Error al actualizar la reservación:', error);
+        res.status(500).json({ error: 'Error al actualizar la reservación'});
+    }
+};
+
+
+
