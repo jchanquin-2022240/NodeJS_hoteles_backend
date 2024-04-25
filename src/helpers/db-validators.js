cccb28de349ea9674becc1ffb25e0059d518b229
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-04-25 00:00:56 -0600

    add validator of date

--- src/helpers/db-validators.js
+++ src/helpers/db-validators.js
@@ -2,3 +2,10 @@ import Reservacion from '../reservations/reservations.model';
 import Habitacion from '../bedrooms/bedrooms.model';
 import Usuario from '../users/users.model';
 
+export const validarFechar = async (fechaInicio, fechaFin) => {
+    if(fechaInicio >= fechaFin) {
+        throw new Error('La fecha de inicio debe ser anterior a la fecha de fin');
+    }
+};
+
+export const validarNumeroHuespedes = async 
