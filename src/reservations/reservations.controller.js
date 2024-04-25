be412630c0ed8e668f2e38a559de087e15ab85f4
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-04-24 22:51:21 -0600

    add in the controller of reservation the function of createReservation

--- src/reservations/reservations.controller.js
+++ src/reservations/reservations.controller.js
@@ -1,100 +1,35 @@
 import { response } from "express";
 import Reservacion from "./reservations.model.js";
-import Habitacion from "../bedrooms/bedrooms.model.js";
+import Habitacion from "../bedrooms/bedrooms.model.js"
+import Usuario from "../users/users.model.js";
 
-export const reservacionPost = async (req, res = response) => {
+export const crearReservacion = async (req, res = response) => {
     try {
-        const { habitacion, fechaInicio, fechaFin, ...resto } = req.body;
+        const { habitacionId, fechaInicio, fechaFin, huespedes, pago} = req.body;
 
-        const habitacionExistente = await Habitacion.findById(habitacion);
-
-        if (!habitacionExistente) {
-            return res.status(404).json({ error: 'Room not found' });
+        const habitacionExistente = await Habitacion.findById(habitacionId);
+        if(!habitacionExistente){
+            return res.status(404).json({ error: 'Habitacion no encontrada' });
         }
 
-        const reservacion = new Reservacion({ habitacion, fechaInicio, fechaFin, ...resto });
+        const usuarioId = req.usuario.id;
+        if (!usuarioId){
+            return res.status(401).json({ error: 'Usuario no autenticado'});
+        }
 
+        const reservacion = new Reservacion({
+            usuario: usuarioId,
+            Habitacion: habitacionId,
+            fechaInicio,
+            fechaFin,
+            huespedes,
+            pago
+        });
         await reservacion.save();
 
-        res.status(200).json({
-            reservacion
-        });
+        res.status(201).json({ msg: 'Reservación creada exitosamente', reservacion});
     } catch (error) {
-        console.error('Error creating reservation:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
-
-export const reservacionPut = async (req, res = response) => {
-    try {
-        const { id } = req.params;
-        const { _id, ...resto } = req.body;
-
-        const reservacionActualizada = await Reservacion.findByIdAndUpdate(id, resto);
-
-        res.status(200).json({
-            msg: 'The reservation was updated successfully.',
-            reservacion: reservacionActualizada
-        });
-    } catch (error) {
-        console.error('Error updating reservation:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
-
-export const reservacionDelete = async (req, res = response) => {
-    try {
-        const { id } = req.params;
-        
-        const reservacionToDelete = await Reservacion.findById(id);
-
-        if (!reservacionToDelete) {
-            return res.status(404).json({ error: 'Reservation not found' });
-        }
-
-        await Reservacion.findByIdAndDelete(id);
-
-        res.status(200).json({
-            msg: 'The reservation was successfully deleted.',
-            reservacion: reservacionToDelete
-        });
-    } catch (error) {
-        console.error('Error deleting reservation:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
-
-export const reservacionGet = async (req, res = response) => {
-    try {
-        const reservacion = await Reservacion.find();
-
-        res.json({
-            reservacion
-        });
-    } catch (error) {
-        console.error('Error fetching reservations:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
-
-export const getHabitacionByReservacion = async (req, res = response) => {
-    try {
-        const reservacionId = req.params.id;
-
-        const reservacion = await Reservacion.findById(reservacionId).populate('habitacion');
-
-        if (!reservacion) {
-            return res.status(404).json({ error: 'Reservation not found' });
-        }
-
-        const { habitacion } = reservacion;
-
-        res.status(200).json({
-            reservacion,
-            habitacion
-        });
-    } catch (error) {
-        console.error('Error fetching room by reservation:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error('Error al crear la reservación:', error);
+        res.status(500).json({ error: 'Error al crear la reservación'});
     }
 }
