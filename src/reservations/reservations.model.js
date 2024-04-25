497018d05968b1ac8640c54c9a20c545838f2575
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-04-24 22:08:10 -0600

    fix model of reservations and add atributes

--- src/reservations/reservations.model.js
+++ src/reservations/reservations.model.js
@@ -1,23 +1,36 @@
 import mongoose, { Schema } from 'mongoose';
 
 const ReservacionSchema = mongoose.Schema({
-    fechaInicio: {
-        type: Date,
-        required: [true, 'Start date is required'],
-    },
-    fechaFin: {
-        type: Date,
-        required: [true, 'End date is required'],
+    usuario: {
+        type: Schema.Types.objectId,
+        ref: 'Usuario',
+        required: [true, 'User reference is mandatory'],
     },
     habitacion: {
-        type: Schema.Types.ObjectId,
+        type: Schema.Types.objectId,
         ref: 'Habitacion',
-        required: [true, 'Room is required'],
+        required: [true, 'Room reference is mandatory'],
     },
-    usuario: {
-        type: Schema.Types.ObjectId,
-        ref: 'Usuario',
-        required: [true, 'User is required'],
+    fechaInicio: {
+        type: Date,
+        required: [true, 'Start date is mandatory'],
+    },
+    fechaFin: { 
+        type: Date,
+        required: [true, 'End date is mandatory'],
+    },
+    huespedes: {
+        type: Number,
+        required: [true, 'Number of guests is required'],
+    },
+    estado: {
+        type: String,
+        enum: ['pendiente', 'confirmada','cancelada'],
+        default: 'pendiente',
+    },
+    pago:{
+        type: Number,
+        required: [true, 'Payment is mandatory'],
     }
 });
 
