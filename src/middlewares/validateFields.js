9eb16984492d69b4a442ea197741e9c7b9eecd2d
Jose Tejaxun <jtejaxun-2022229@kinal.edu.gt>
2024-04-27 22:09:38 -0600

    se crean validaciones de campos y de atributos de hotel

---
+++ src/middlewares/validateFields.js
@@ -0,0 +1,38 @@
+import Hotel from '../hoteles/hotel.model.js'
+
+export const existsHotel = async (id = '') => {
+
+    const hotelExists = await Hotel.findOne({ id });
+    if (hotelExists) {
+        throw new Error(`Hotel with ${id} does not exists.`);
+    }
+}
+
+export const existsNameHotel = async (nameHotel = '') => {
+
+    const nameHotelExists = await Hotel.findOne({ nameHotel });
+    if (nameHotelExists) {
+        throw new Error(`The hotel called ${nameHotel} is already registered.`);
+    }
+}
+
+export const existsLocation = async (location = '') => {
+
+    const locationExists = await Hotel.findOne({ location });
+    if (locationExists) {
+        throw new Error(`The location ${location} is already in use, by another hotel.`)
+    }
+}
+
+export const hotelStatus = async (id = '') => {
+
+    try {
+        const hotel = await Hotel.findById(id);
+
+        if (!hotel.status) {
+            throw new Error(`Hotel with ${id} is alredy disable`);
+        }
+    } catch (error) {
+        throw new Error("Error:", error.message);
+    }
+}
