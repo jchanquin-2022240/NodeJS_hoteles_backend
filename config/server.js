8edaf60b05c9787036a20875cac051cb58b4c2df
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-04-30 14:06:24 -0600

    se agrego arranque en package.json dev, y se elimino las rutas de server

--- config/server.js
+++ config/server.js
@@ -5,13 +5,12 @@ import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import { dbConnection } from './mongo.js'
-import hotelRoutes from '../src/hoteles/hotel.routes.js'
+
 
 class Server{
     constructor(){
         this.app = express()
         this.port = process.env.PORT
-        this.hotelPath = '/api/v1/hotel'
 
         this.middlewares()
         this.conectarDB()
@@ -31,51 +30,7 @@ class Server{
     }
 
     routes(){
-        this.app.use(this.hotelPath, hotelRoutes)
-    }
-
-    listen(){
-        this.app.listen(this.port, () => {
-            console.log('Server running on port ', this.port)
-        })
-    }
-}
-
-export default Server
-'use strict'
-
-import express from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
-import morgan from 'morgan'
-import { dbConnection } from './mongo.js'
-import userRoutes from '../src/user/user.routes.js'
-
-class Server{
-    constructor(){
-        this.app = express()
-        this.port = process.env.PORT
-        this.authPath = '/api/v1/auth'
-
-        this.middlewares()
-        this.conectarDB()
-        this.routes()
-    }
-
-    async conectarDB(){
-        await dbConnection()
-    }
-
-    middlewares(){
-        this.app.use(express.urlencoded({extended: false}))
-        this.app.use(cors())
-        this.app.use(express.json())
-        this.app.use(helmet())
-        this.app.use(morgan('dev'))
-    }
-
-    routes(){
-        this.app.use(this.authPath, userRoutes);
+        
     }
 
     listen(){
