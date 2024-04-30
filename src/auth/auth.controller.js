16f436a5a2d135c80ae60a90644dcad1628d27ae
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-04-30 14:02:06 -0600

    se agrego auth.controller y auth.routes

---
+++ src/auth/auth.controller.js
@@ -0,0 +1,44 @@
+import bcryptjs from 'bcryptjs';
+import Usuario from '../user/user.model.js'
+import { generarJWT } from '../helpers/generate-jwt.js'; 
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+  try {
+    const usuario = await Usuario.findOne({ email });
+
+    if (!usuario) {
+      return res.status(400).json({
+        msg: "Credenciales incorrectas, Correo no existe en la base de datos",
+      });
+    }
+
+    if (!usuario.status) {
+      return res.status(400).json({
+        msg: "El usuario no existe en la base de datos",
+      });
+    }
+
+    const validPassword = bcryptjs.compareSync(password, usuario.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "La contraseña es incorrecta",
+      });
+    }
+
+    const token = await generarJWT( usuario.id);
+
+    res.status(200).json({
+      msg: 'Login Ok!!!',
+      usuario,
+      token
+    });
+
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      msg: "Comuniquese con el administrador",
+    });
+  }
+}
